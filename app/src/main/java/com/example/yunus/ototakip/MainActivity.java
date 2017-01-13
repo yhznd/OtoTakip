a069702ef490af4cfc5ed09385940a7994d368f8
Yunus Haznedar <yunushaznedar@gmail.com>
2017-01-14 00:51:26 +0300

    First Commit
    BSM 497

---
+++ app/src/main/java/com/example/yunus/ototakip/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yunus.ototakip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Merhaba ben Yunus'tan clone edildim.
+    }
+}
