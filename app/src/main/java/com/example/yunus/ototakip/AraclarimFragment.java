ca93abaf649687fb16417e74cf3b27f1ace704b8
Yunus Haznedar <yunushaznedar@gmail.com>
2017-01-24 22:58:27 +0300

    Araç Ekleme Sayfası
    Araç ekleme sayfası eklendi, snackbar kullanımı buttonlara eklendi.

--- app/src/main/java/com/example/yunus/ototakip/AraclarimFragment.java
+++ app/src/main/java/com/example/yunus/ototakip/AraclarimFragment.java
@@ -1,8 +1,10 @@
 package com.example.yunus.ototakip;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -22,7 +24,7 @@ public class AraclarimFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Araç Ekleme sayfasına yönlendirileceksiniz.", Snackbar.LENGTH_LONG)/.setAction("Action", null).show();
+                startActivity(new Intent(getContext(),AracEkle.class));
             }
         });
 
