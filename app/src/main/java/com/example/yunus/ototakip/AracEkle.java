eae185e467e4648d00420301b674ceb50e9d89a5
Yunus Haznedar <yunushaznedar@gmail.com>
2017-05-01 01:50:48 +0300

    Son Kilometre Taşı v2
    Düzenlemeler

--- app/src/main/java/com/example/yunus/ototakip/AracEkle.java
+++ app/src/main/java/com/example/yunus/ototakip/AracEkle.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -189,6 +190,7 @@ public class AracEkle extends AppCompatActivity implements View.OnClickListener{
         araba =new Araba(userMail,aracModeli,aracKaskoTrhi,aracTrafikTrhi,aracMuayeneTrhi,aracSigortaTrhi);
         reference=database.getReference("Arabalar").child(userId).child(aracPlakasi);
         reference.setValue(araba);
+        Toast.makeText(AracEkle.this,"Araç başarıyla eklendi!",Toast.LENGTH_LONG).show();
 
     }
 
